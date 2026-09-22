8c968bc0e1e975b241ce275258dca8a9cc38d20b
guilty442-source <guilty442-source@users.noreply.github.com>
2026-09-22 18:38:39 +0800

    chore: remove temp toolhost build helper (diagnostic artifact)

--- native/test_suites/_toolhost_only.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "E:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat" >nul || exit /b 1
-cl /nologo /std:c++17 /utf-8 /O2 /EHsc /I"E:\GPTBridge\native\include" /Fe:E:\GPTBridge\native\test_suites\bin\tool_host_suite.exe /Fo:E:\GPTBridge\native\test_suites\bin\ "E:\GPTBridge\native\test_suites\suite_tool_host.cpp" "E:\GPTBridge\native\tool_runtime\tool_host.cpp" "E:\GPTBridge\native\tool_runtime\governed_tool_ws.cpp" "E:\GPTBridge\native\tool_runtime\transport_proxy_client.cpp" "E:\GPTBridge\native\tool_runtime\sidecar_transport.cpp" "E:\GPTBridge\native\core\governed_tool.c" "E:\GPTBridge\native\core\system_rescue.c"
-exit /b %errorlevel%
